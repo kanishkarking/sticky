f25db37c0e7a63d55395b1f215730a39da1688da
kanishkarking <146754504+kanishkarking@users.noreply.github.com>
2023-10-02 22:36:01 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class Sample{
 
 public static void main(String[] args){
 
-	System.out.println("Vera level pandringa ji.");
+	System.out.println(" Again and Again Vera level pandringa ji.");
 }
 
 }
